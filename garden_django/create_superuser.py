962145fc51d1acc4d8deec0829f2fdb5ff4d3c0a
Arman Tuganbaev <arman.tuganbaev@gmail.com>
2024-05-04 14:30:37 +0200

    Make CustomAdmin to use system_api_too

--- garden_django/create_superuser.py
+++ garden_django/create_superuser.py
@@ -15,7 +15,7 @@ email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "adminpassword")
 
 if not CustomUser.objects.filter(username=username).exists():
-    CustomUser.objects.create_superuser(username, email, password)
+    CustomUser.objects.create_superuser(username, email, password, use_system_api_key = True)
     print(f"Superuser {username} created.")
 else:
     print(f"Superuser {username} already exists.")
